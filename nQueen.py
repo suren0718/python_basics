ed7e5e55eaa9926bd9c26314e3eb30b8b9d5faed
suren0718 <surendransivakumar2004@gmail.com>
2025-08-21 14:10:52 +0530

    v2

--- nQueen.py
+++ nQueen.py
@@ -1,12 +1,28 @@
-def nQueen(n):
-  dp=[[0] * n for _ in range(n)]
-  row=len(dp)
-  col=len(dp[0])  
-  
-  for r in range(row):
-    for c in range(col):
-      if 
+ways=0
 
+def totalNqueens(n):
+  global ways
+  ways=0
+  rowf= [False] * n
+  colf =[False] * n
+  t1df = [False] *(2 * n + 1)
+  t2df = [False] *(2 * n + 1)
+  fill(n, 0, rowf, colf, t1df, t2df)
+  return ways 
 
-n=4
-print(nQueen(n))
+def fill(n, row, rowf, colf, t1df, t2df):
+  global ways
+  if row == n:
+    ways += 1
+    return
+
+  for col in range(n):
+    if not rowf[row] and not colf[col] and not t1df[row -col + n] and not t2df[row + col]:
+
+      rowf[row] = colf[col] = t1df[row-col +n] = t2df[row + col] =True
+
+      fill(n, row+1, rowf, colf, t1df, t2df)
+
+      rowf[row] = colf[col]= t1df[row-col+n] = t2df[row+col] = False
+
+print(totalNqueens(4))
