651e8534494579c43ffc783e4fa5e31e9b29fe53
suren0718 <surendransivakumar2004@gmail.com>
2025-08-22 10:19:53 +0530

    v2

--- numVisPeople.py
+++ numVisPeople.py
@@ -1,18 +1,19 @@
-class solution:
-  def seenPeople(heights):
-    n=len(heights)
-    stack=[]
-    res=[[0] * n]
-    for i in range(n-1, -1, -1):
-      while stack:
-        count=0
-        if stack[-1]<heights[i]:
-          count+=1
-          stack.pop()
-        else: 
-          break
-      res[i]=count
-      stack.push(heights[i])
+def seenPeople(heights):
+    n = len(heights)
+    stack = []
+    res = [0] * n
+    for i in range(n - 1, -1, -1):
+        count = 0
+        while stack:
+            count += 1
+            if stack[-1] < heights[i]:
+                stack.pop()
+            else:
+                break
+        res[i] = count
+        stack.append(heights[i])
     return res
-heights=[10,6, 8,5, 11,9]
+
+
+heights = [10, 6, 8, 5, 11, 9]
 print(seenPeople(heights))
